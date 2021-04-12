05c8cdc517bbf3c73844108a40f45107319b82fa
CHRIS_LEE <chrismlee87@gmail.com>
2021-04-12 16:50:37 -0700

    users and message complete

--- src/routes/message.js
+++ src/routes/message.js
@@ -15,10 +15,28 @@ router.get('/:messageId', (req, res) => {
 })
 
 /** Route to add a new message. */
+// router.post('/', (req, res) => {
+//     return res.send({
+//         message: 'Create new message',
+//         data: req.body
+//     })
+// })
+
 router.post('/', (req, res) => {
-    return res.send({
-        message: 'Create new message',
-        data: req.body
+    let message = new Message(req.body)
+    message.save()
+    .then(message => {
+        return User.findById(message.author)
+    })
+    .then(user => {
+        console.log(user)
+        user.messages.unshift(message)
+        return user.save()
+    })
+    .then(_ => {
+        return res.send(message)
+    }).catch(err => {
+        throw err.message
     })
 })
 
